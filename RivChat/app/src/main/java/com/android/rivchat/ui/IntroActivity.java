c92f1fb978451900f1de8ea94069c5919ddeaa3d
octacode <allblue.shasha@gmail.com>
2018-02-10 18:32:28 +0530

    Integrated google pay api

--- RivChat/app/src/main/java/com/android/rivchat/ui/IntroActivity.java
+++ RivChat/app/src/main/java/com/android/rivchat/ui/IntroActivity.java
@@ -1,19 +1,51 @@
 package com.android.rivchat.ui;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 
 import com.android.rivchat.R;
 import com.android.rivchat.util.Preferences;
 import com.github.paolorotolo.appintro.AppIntro;
 import com.github.paolorotolo.appintro.AppIntroFragment;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.wallet.AutoResolveHelper;
+import com.google.android.gms.wallet.CardRequirements;
+import com.google.android.gms.wallet.IsReadyToPayRequest;
+import com.google.android.gms.wallet.PaymentData;
+import com.google.android.gms.wallet.PaymentDataRequest;
+import com.google.android.gms.wallet.PaymentMethodTokenizationParameters;
+import com.google.android.gms.wallet.PaymentsClient;
+import com.google.android.gms.wallet.TransactionInfo;
+import com.google.android.gms.wallet.Wallet;
+import com.google.android.gms.wallet.WalletConstants;
+
+import java.util.Arrays;
 
 public class IntroActivity extends AppIntro {
+
+    private PaymentsClient mPaymentsClient;
+    private int LOAD_PAYMENT_DATA_REQUEST_CODE = 0;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mPaymentsClient =
+                Wallet.getPaymentsClient(
+                        this,
+                        new Wallet.WalletOptions.Builder()
+                                .setEnvironment(WalletConstants.ENVIRONMENT_TEST)
+                                .build());
+
+        isReadyToPay();
+
         addSlide(AppIntroFragment.newInstance("1", "", R.drawable.circle_background, getResources().getColor(R.color.colorPrimary)));
         addSlide(AppIntroFragment.newInstance("2", "", R.drawable.circle_background, getResources().getColor(R.color.colorPrimary)));
         addSlide(AppIntroFragment.newInstance("3", "", R.drawable.circle_background, getResources().getColor(R.color.colorPrimary)));
@@ -22,26 +54,86 @@ public class IntroActivity extends AppIntro {
         setFlowAnimation();
     }
 
+    private void isReadyToPay() {
+        IsReadyToPayRequest request =
+                IsReadyToPayRequest.newBuilder()
+                        .addAllowedPaymentMethod(WalletConstants.PAYMENT_METHOD_CARD)
+                        .addAllowedPaymentMethod(WalletConstants.PAYMENT_METHOD_TOKENIZED_CARD)
+                        .build();
+        Task<Boolean> task = mPaymentsClient.isReadyToPay(request);
+        task.addOnCompleteListener(
+                new OnCompleteListener<Boolean>() {
+                    public void onComplete(Task<Boolean> task) {
+                        try {
+                            boolean result = task.getResult(ApiException.class);
+                            if (result) {
+                                // Show Google as payment option.
+                            } else {
+                                // Hide Google as payment option.
+                            }
+                        } catch (ApiException exception) {
+                        }
+                    }
+                });
+    }
+
+    private PaymentDataRequest createPaymentDataRequest() {
+        PaymentDataRequest.Builder request =
+                PaymentDataRequest.newBuilder()
+                        .setTransactionInfo(
+                                TransactionInfo.newBuilder()
+                                        .setTotalPriceStatus(WalletConstants.TOTAL_PRICE_STATUS_FINAL)
+                                        .setTotalPrice("10.00")
+                                        .setCurrencyCode("USD")
+                                        .build())
+                        .addAllowedPaymentMethod(WalletConstants.PAYMENT_METHOD_CARD)
+                        .addAllowedPaymentMethod(WalletConstants.PAYMENT_METHOD_TOKENIZED_CARD)
+                        .setCardRequirements(
+                                CardRequirements.newBuilder()
+                                        .addAllowedCardNetworks(
+                                                Arrays.asList(
+                                                        WalletConstants.CARD_NETWORK_AMEX,
+                                                        WalletConstants.CARD_NETWORK_DISCOVER,
+                                                        WalletConstants.CARD_NETWORK_VISA,
+                                                        WalletConstants.CARD_NETWORK_MASTERCARD))
+                                        .build());
+
+        PaymentMethodTokenizationParameters params =
+                PaymentMethodTokenizationParameters.newBuilder()
+                        .setPaymentMethodTokenizationType(
+                                WalletConstants.PAYMENT_METHOD_TOKENIZATION_TYPE_PAYMENT_GATEWAY)
+                        .addParameter("gateway", "yourGateway")
+                        .addParameter("gatewayMerchantId", "yourMerchantIdGivenFromYourGateway")
+                        .build();
+
+        request.setPaymentMethodTokenizationParameters(params);
+        return request.build();
+    }
+
     @Override
     public void onSkipPressed(Fragment currentFragment) {
         super.onSkipPressed(currentFragment);
         //Payment gateway
-
-        //OnFailure
-        onFailure();
-
-        //On success
-        onSuccess();
+        initiatePayment();
     }
 
     @Override
     public void onDonePressed(Fragment currentFragment) {
         super.onDonePressed(currentFragment);
         //Payment gateway
-        //OnFailure
-        onFailure();
-        //On success
-        onSuccess();
+        initiatePayment();
+    }
+
+    private void initiatePayment() {
+        PaymentDataRequest request = createPaymentDataRequest();
+        if (request != null) {
+            AutoResolveHelper.resolveTask(
+                    mPaymentsClient.loadPaymentData(request),
+                    this,
+                    // LOAD_PAYMENT_DATA_REQUEST_CODE is a constant value
+                    // you define.
+                    LOAD_PAYMENT_DATA_REQUEST_CODE);
+        }
     }
 
     private void onSuccess() {
@@ -51,6 +143,29 @@ public class IntroActivity extends AppIntro {
     }
 
     private void onFailure() {
-        startActivity(new Intent (this, SplashActivity.class));
+        startActivity(new Intent(this, SplashActivity.class));
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (resultCode) {
+            case Activity.RESULT_OK:
+                PaymentData paymentData = PaymentData.getFromIntent(data);
+                String token = paymentData.getPaymentMethodToken().getToken();
+                onSuccess();
+                break;
+            case Activity.RESULT_CANCELED:
+                onFailure();
+                break;
+            case AutoResolveHelper.RESULT_ERROR:
+                Status status = AutoResolveHelper.getStatusFromIntent(data);
+                onFailure();
+                // Log the status for debugging.
+                // Generally, there is no need to show an error to
+                // the user as the Google Pay API will do that.
+                break;
+            default:
+                // Do nothing.
+        }
     }
 }
--- RivChat/app/src/main/java/com/android/rivchat/ui/SplashActivity.java
+++ RivChat/app/src/main/java/com/android/rivchat/ui/SplashActivity.java
@@ -6,6 +6,7 @@ import com.android.rivchat.util.Preferences;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class SplashActivity extends AppCompatActivity {
@@ -13,18 +14,20 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(getClass().getSimpleName(), "HELLO SPLASH");
+
         setContentView(R.layout.activity_spalsh);
         if (Preferences.getFirstRun(this)) {
             //First Run Activity
             startActivity(new Intent(this, IntroActivity.class));
         }
 
-        if (!Preferences.getPaid(this)){
+        else if (!Preferences.getPaid(this)){
             Toast.makeText(this, "You haven't paid yet!", Toast.LENGTH_SHORT).show();
             startActivity(new Intent(this, IntroActivity.class));
         }
 
-        if (Preferences.getPaid(this))
+        else if (Preferences.getPaid(this))
             startActivity(new Intent(this, LoginActivity.class));
     }
 }
